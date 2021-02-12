6de7b3363c3facd3863b4d5483be73bf78f1b743
Negrativo <49249175+Negrativo@users.noreply.github.com>
2021-02-12 16:20:17 -0300

    Alteração do fonte
    Alteração do fonte, em busca de mudar o projeto de multiDevice para VCL, com isso possibilita a utilização de DBGrid

--- fontes/Cadastro/Cliente.pas
+++ fontes/Cadastro/Cliente.pas
@@ -16,14 +16,21 @@ type
   TForm_Clientes = class(TForm)
     Panel3: TPanel;
     Panel4: TPanel;
-    Grid_cliente: TGrid;
     D_cliente: TDataSource;
     Q_cliente: TFDQuery;
-    nome: TColumn;
-    telefone: TColumn;
-    email: TColumn;
-    cpf: TColumn;
+    StringGrid1: TStringGrid;
+    StringColumn1: TStringColumn;
+    T_cliente: TFDTable;
+    T_clientennumeclie: TIntegerField;
+    T_clientecnomeclie: TStringField;
+    T_clientecmailclie: TStringField;
+    T_clientec_cpfclie: TStringField;
+    T_clientecendeclie: TStringField;
+    T_clientednascclie: TDateField;
+    T_clientecsexoclie: TStringField;
+    T_clientecfoneclie: TStringField;
     procedure Image5Click(Sender: TObject);
+    procedure Image_salvarCClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -44,15 +51,37 @@ var
   i :Integer;
 begin
   Q_cliente.Open();
+
   while Q_cliente.Eof do begin
-    Grid_cliente.Columns[0] := Q_cliente.FieldByName('cnomeclie').AsString;
-    Grid_cliente.Columns[1] := Q_cliente.FieldByName('cfoneclie').AsString;
-    Grid_cliente.Columns[2] := Q_cliente.FieldByName('cmailclie').AsString;
-    Grid_cliente.Columns[3] := Q_cliente.FieldByName('c_cpfclie').AsString;
+    Grid_cliente.ColumnIndex.Parse(Q_cliente.ToString);
+    //Grid_cliente.Columns[1] := Q_cliente.FieldByName('cfoneclie').AsString;
+    //Grid_cliente.Columns[2] := Q_cliente.FieldByName('cmailclie').AsString;
+   // Grid_cliente.Columns[3] := Q_cliente.FieldByName('c_cpfclie').AsString;
     Q_cliente.Next;
-    i := i + 1;
+    //i := i + 1;
   end;
   Q_cliente.Close;
+end;
+
+procedure TForm_Clientes.Image_salvarCClick(Sender: TObject);
+begin
+ Q_cliente.Post;
+end;
+
+
+procedure Tform_Clientes.associar_campos;
+begin
+
+    T_Cliente.FieldByName('cnomeclie').Value := e_nomeC.Text;
+  T_Cliente.FieldByName('cmailclie').Value := e_mailC.Text;
+  T_Cliente.FieldByName('c_cpfclie').Value := e_cpfC.Text;
+  T_Cliente.FieldByName('cendeclie').Value := e_endeC.Text;
+  T_Cliente.FieldByName('dnascclie').Value := e_nascC.Text;
+  T_Cliente.FieldByName('dfoneclie').Value := e_foneC.Text;
+  if pop_sexo.ItemIndex = 1 then
+    T_Cliente.FieldByName('csexoclie').Value := 'M'
+  else
+    T_Cliente.FieldByName('csexoclie').Value := 'F';
 end;
 
 end.
